a7b8610c65aa2b9da3b7959bd36d20f4eadd2c2a
Ádám Horváth <adam.horvath@attrecto.com>
2020-09-01 08:37:57 +0200

    db config set

--- config/database.js
+++ config/database.js
@@ -2,6 +2,6 @@
  * Created by horvath on 2017. 05. 08.
  */
 module.exports = {
-    'secret': 'myCostSecret123',
-    'database': 'mongodb://localhost/node-cost'
+    'secret': 'horvath7117',
+    'database': 'mongodb://horvath@localhost:27017/node-cost'
 };
