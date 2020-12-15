cc514aab324a379bff694df16b5fd44cd534a51d
Ádám Horváth <adam.horvath@attrecto.com>
2020-12-15 09:01:22 +0100

    async-await

--- app/geolocation/geolocation.js
+++ app/geolocation/geolocation.js
@@ -1,26 +1,25 @@
 let nearbyCities = require("nearby-cities");
 
-let getCity = (req, res) => {
+let getCity = async (req, res) => {
   const input = req.query;
   if (!input || !input.lat || !input.lng) {
     console.log(5177, "No location data provided.");
     return res.json({ success: false, msg: "No location data provided." });
   }
   const { lat, lng } = req.query;
-  nearbyCities({ latitude: lat, longitude: lng })
-    .then(result => {
-      return res.status(200).send({
-        result,
-        msg:
-          result && result.length
-            ? `A visszaadott városok száma: ${result.length}`
-            : "Nincs találat."
-      });
-    })
-    .catch(err => {
-      console.log(5178, err.message);
-      return res.status(403).send({ success: false, msg: err.message });
+  try {
+    const cities = await nearbyCities({ latitude: lat, longitude: lng });
+    return res.status(200).send({
+      cities,
+      msg:
+        cities && cities.length
+          ? `A visszaadott városok száma: ${cities.length}`
+          : "Nincs találat."
     });
+  } catch (err) {
+    console.log(5178, err.message);
+    return res.status(403).send({ success: false, msg: err.message });
+  }
 };
 
 module.exports = {
